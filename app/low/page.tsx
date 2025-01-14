1e8c693a24214be0906e72c1a769c427d3eb3474
namish <namish@tuta.io>
2025-01-14 21:18:57 +0530

    metadata

--- app/low/page.tsx
+++ app/low/page.tsx
@@ -1,5 +1,36 @@
+import type { Metadata } from "next";
 import Generate from "../components/generate";
 import { promises as fs } from 'fs';
+
+
+export const metadata: Metadata = {
+    title: "C and Rust Roadmap",
+    description: "Low level programming languages",
+    twitter: {
+        card: 'summary_large_image',
+        title: 'C and Rust Roadmap',
+        description: 'Low level programming languages',
+        images: ['https://i.imgur.com/BwGG7Ch.png'],
+    },
+    openGraph: {
+        title: 'C and Rust Roadmap',
+        description: 'Low level programming languages',
+        images: [
+            {
+                url: 'https://i.imgur.com/BwGG7Ch.png', 
+                width: 800,
+                height: 600,
+            },
+            {
+                url: 'https://i.imgur.com/BwGG7Ch.png',
+                width: 1800,
+                height: 1600,
+                alt: 'My custom alt',
+            },
+        ],
+    }
+}
+
 
 export default async function Low() {
   const file = await fs.readFile(process.cwd() + '/app/data/low.json', 'utf8');
--- app/web/page.tsx
+++ app/web/page.tsx
@@ -1,8 +1,38 @@
+import { Metadata } from "next";
 import Generate from "../components/generate";
 import { promises as fs } from 'fs';
 
+export const metadata: Metadata = {
+    title: "Web Development Roadmap",
+    description: "Full stack development with JavaScript and NextJS",
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Web Development Roadmap',
+        description: 'Full stack development with JavaScript and NextJS',
+        images: ['https://i.imgur.com/sy7ZTsr.png'],
+    },
+    openGraph: {
+        siteName: "One Semester",
+        title: 'Web Development Roadmap',
+        description: 'Full stack development with JavaScript and NextJS',
+        images: [
+            {
+                url: 'https://i.imgur.com/sy7ZTsr.png', 
+                width: 800,
+                height: 600,
+            },
+            {
+                url: 'https://i.imgur.com/sy7ZTsr.png',
+                width: 1800,
+                height: 1600,
+                alt: 'My custom alt',
+            },
+        ],
+    }
+}
+
 export default async function Web() {
-  const file = await fs.readFile(process.cwd() + '/app/data/web.json', 'utf8');
-  const data = JSON.parse(file) as LearningPlan;
-  return <Generate learningPlan={data}/>
+    const file = await fs.readFile(process.cwd() + '/app/data/web.json', 'utf8');
+    const data = JSON.parse(file) as LearningPlan;
+    return <Generate learningPlan={data} />
 }
