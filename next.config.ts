67bcb125dffe384ca5bb1730670da5691b9b38c0
namish <namish@tuta.io>
2025-01-15 15:48:35 +0530

    reverr

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */ 
-  basePath: "/semester",
-  output: "export",  // <=== enables static exports
-
 };
 
 export default nextConfig;
