5de64b446d4635a5a5151b08f3b924822933066e
namish <namish@tuta.io>
2025-01-17 16:25:46 +0530

    link font in html

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,14 +41,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Jersey+25&display=swap" rel="stylesheet" />
+      </head>
       <body
         className={`antialiased crt bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:48px_48px] [mask-image:radial-gradient(ellipse_80%_80%_at_50%_50%,#000_60%,transparent_100%)]`}
       >
         {children}
-        <HelpWindow/>
-        <PathSelector/>
-        <VimMotions/>
-        <SearchBar/>
+        <HelpWindow />
+        <PathSelector />
+        <VimMotions />
+        <SearchBar />
         <div className="invisible md:visible fixed bottom-4 left-4 text-xl text-neutral-700">space + h for keyboard shortcuts</div>
       </body>
     </html>
