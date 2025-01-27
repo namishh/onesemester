e9d6000a25b7484ba89da3a3a55b2540df2301a6
namish <namish@tuta.io>
2025-01-28 01:34:52 +0530

    animated progressbar

--- app/components/progressbar.tsx
+++ app/components/progressbar.tsx
@@ -6,24 +6,31 @@ export default function ProgressBar({ total }: { total: number }) {
 	const progressPercentage = total > 0 ? (completedTasks / total) * 100 : 0;
 
 	return (
-		<div className="fixed w-screen z-[10] h-2 md:h-3">
+		<div className="fixed w-screen z-[100] h-2 md:h-3 overflow-hidden">
 			<div
 				style={{
 					width: `${progressPercentage}%`,
 					background: `
-				repeating-linear-gradient(
-				45deg,
-					#10b981, 
-					#10b981 10px,
-					#059669 10px, 
-					#059669 20px
-				)
-			`,
-			backgroundSize: '28.28px 28.28px',
-		}}
-
+						repeating-linear-gradient(
+							45deg,
+							#10b981, 
+							#10b981 10px,
+							#059669 10px, 
+							#059669 20px
+						)
+					`,
+					backgroundSize: '28.28px 28.28px',
+					animation: 'pan 1s linear infinite'
+				}}
 				className="h-full transition-width"
 			></div>
+			
+			<style jsx global>{`
+				@keyframes pan {
+					from { background-position: 0 0; }
+					to { background-position: -28.28px 0; }
+				}
+			`}</style>
 		</div>
 	);
 }
